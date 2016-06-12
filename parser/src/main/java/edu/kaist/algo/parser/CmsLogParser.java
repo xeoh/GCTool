fd652ac0a4f93a19310fd203ab1e350ebd5350cd
ducky <pacifings@kaist.ac.kr>
2016-06-12 19:34:53 +0900

    [#67] Apply reviews. (Add more tests, fix code styles.)

--- parser/src/main/java/edu/kaist/algo/parser/CmsLogParser.java
+++ parser/src/main/java/edu/kaist/algo/parser/CmsLogParser.java
@@ -8,6 +8,7 @@
 
 package edu.kaist.algo.parser;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
 
@@ -31,7 +32,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -47,7 +47,8 @@ public class CmsLogParser {
   private static final Pattern WRITER_THREAD = Pattern.compile("<writer thread='(\\d+)'/>");
   private static final Pattern MULTI_LINE = Pattern.compile(".*GC.*\\[CMS$");
 
-  private int currentThread;
+  @VisibleForTesting
+  int currentThread;
   private final Map<Integer, String> threadToIncompleteLine = new HashMap<>();
   private final ParseRunner<Object> parseRunner;
 
@@ -82,17 +83,13 @@ public class CmsLogParser {
         .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
   }
 
-  private GcEvent parseLine(String line) {
+  @VisibleForTesting
+  GcEvent parseLine(String line) {
     if (line.startsWith("<writer")) {
-      final Matcher m = WRITER_THREAD.matcher(line);
-      if (m.matches()) {
-        currentThread = Integer.parseInt(m.group(1));
-      }
+      currentThread = parseWriterThreadId(line);
+    } else if (MULTI_LINE.matcher(line).matches()) {
+      threadToIncompleteLine.put(currentThread, line);
     } else {
-      if (MULTI_LINE.matcher(line).matches()) {
-        threadToIncompleteLine.put(currentThread, line);
-        return null;
-      }
       if (threadToIncompleteLine.containsKey(currentThread)) {
         final String previousLine = threadToIncompleteLine.remove(currentThread);
         line = previousLine + line;
@@ -102,60 +99,80 @@ public class CmsLogParser {
     return null;
   }
 
-  private GcEvent parseGcEvent(final String line) {
-    final ParsingResult<Object> result = parseRunner.run(line);
-    if (result.matched) {
-      final GcEventNode node = (GcEventNode) result.resultValue;
-      final GcEvent.LogType logType = convertLogType(node);
-      final String typeDetails =
-          Stream.concat(Stream.of(node), node.children().stream())
-              .map(GcEventNode::typeAndDetail)
-              .collect(Collectors.joining("; "));
-      final GcEvent.Builder builder = GcEvent.newBuilder()
-          .setThread(currentThread)
-          .setTimestamp(MoreObjects.firstNonNull(node.timestamp(), 0L))
-          .setLogType(logType)
-          .setPauseTime(MoreObjects.firstNonNull(node.elapsedTime(), 0.0))
-          .setUserTime(MoreObjects.firstNonNull(node.user(), 0.0))
-          .setSysTime(MoreObjects.firstNonNull(node.sys(), 0.0))
-          .setRealTime(MoreObjects.firstNonNull(node.real(), 0.0))
-          .setCmsCpuTime(MoreObjects.firstNonNull(node.cmsCpuTime(), 0.0))
-          .setCmsWallTime(MoreObjects.firstNonNull(node.cmsWallTime(), 0.0))
-          .setTypeDetail(typeDetails);
-      if (logType == GcEvent.LogType.CMS_FINAL_REMARK) {
-        final Optional<GcEventNode> weakRefsProcessingEvent = node.children().stream()
-            .filter(e -> StringUtils.equals(e.type(), "weak refs processing"))
-            .findFirst();
-        if (weakRefsProcessingEvent.isPresent()) {
-          builder.setRefTime(MoreObjects.firstNonNull(weakRefsProcessingEvent.get().elapsedTime(), 0.0));
-        }
+  @VisibleForTesting
+  int parseWriterThreadId(String line) {
+    final Matcher m = WRITER_THREAD.matcher(line);
+    if (m.matches()) {
+      try {
+        return Integer.parseInt(m.group(1));
+      } catch (NumberFormatException nfe) {
+        logger.error("writer thread id must be number : " + line, nfe);
       }
-      return builder.build();
     }
-    return null;
+    return -1;
   }
 
-  private GcEvent.LogType convertLogType(final GcEventNode node) {
+  @VisibleForTesting
+  GcEvent parseGcEvent(final String line) {
+    final ParsingResult<Object> result = parseRunner.run(line);
+    if (!result.matched) {
+      return null;
+    }
+    final GcEventNode node = (GcEventNode) result.resultValue;
+    final GcEvent.LogType logType = convertLogType(node);
+    final String typeDetails =
+        Stream.concat(Stream.of(node), node.children().stream())
+            .map(GcEventNode::typeAndDetail)
+            .collect(Collectors.joining("; "));
+    final GcEvent.Builder builder = GcEvent.newBuilder()
+        .setThread(currentThread)
+        .setTimestamp(MoreObjects.firstNonNull(node.timestamp(), 0L))
+        .setLogType(logType)
+        .setPauseTime(MoreObjects.firstNonNull(node.elapsedTime(), 0.0))
+        .setUserTime(MoreObjects.firstNonNull(node.user(), 0.0))
+        .setSysTime(MoreObjects.firstNonNull(node.sys(), 0.0))
+        .setRealTime(MoreObjects.firstNonNull(node.real(), 0.0))
+        .setCmsCpuTime(MoreObjects.firstNonNull(node.cmsCpuTime(), 0.0))
+        .setCmsWallTime(MoreObjects.firstNonNull(node.cmsWallTime(), 0.0))
+        .setTypeDetail(typeDetails);
+    if (logType == GcEvent.LogType.CMS_FINAL_REMARK) {
+      final Optional<GcEventNode> weakRefTimeOption = node.children().stream()
+          .filter(e -> StringUtils.equals(e.type(), "weak refs processing"))
+          .findFirst();
+      if (weakRefTimeOption.isPresent()) {
+        double refTime = MoreObjects.firstNonNull(weakRefTimeOption.get().elapsedTime(), 0.0);
+        builder.setRefTime(refTime);
+      }
+    }
+    return builder.build();
+  }
+
+  @VisibleForTesting
+  GcEvent.LogType convertLogType(final GcEventNode node) {
+    if (StringUtils.startsWith(node.type(), "CMS-concurrent-")) {
+      return GcEvent.LogType.CMS_CONCURRENT;
+    }
     if (StringUtils.equals(node.type(), "Full GC")) {
       return GcEvent.LogType.FULL_GC;
-    } else if (StringUtils.equals(node.type(), "GC")) {
+    }
+    // GC Pause that starts with "GC" can be either one of the following:
+    //   * CMS initial mark
+    //   * CMS remark
+    //   * ParNew (Minor GC)
+    //   * ParNew that ended up as Full GC
+    if (StringUtils.equals(node.type(), "GC")) {
       if (StringUtils.equals(node.detail(), "CMS Initial Mark")) {
         return GcEvent.LogType.CMS_INIT_MARK;
-      } else if (StringUtils.equals(node.detail(), "CMS Final Remark")) {
-        return GcEvent.LogType.CMS_FINAL_REMARK;
-      } else {
-        final List<String> types = node.children().stream()
-            .map(GcEventNode::type).collect(Collectors.toList());
-        final boolean isFullGc = Collections.indexOfSubList(
-            types, ImmutableList.of("ParNew", "CMS")) != -1;
-        if (isFullGc) {
-          return GcEvent.LogType.FULL_GC;
-        } else {
-          return GcEvent.LogType.MINOR_GC;
-        }
       }
-    } else if (StringUtils.startsWith(node.type(), "CMS-concurrent-")) {
-      return GcEvent.LogType.CMS_CONCURRENT;
+      if (StringUtils.equals(node.detail(), "CMS Final Remark")) {
+        return GcEvent.LogType.CMS_FINAL_REMARK;
+      }
+      final List<String> types = node.children().stream()
+          .map(GcEventNode::type).collect(Collectors.toList());
+      // If ParNew ended up in full GC, [CMS ... ] appears in the log.
+      final boolean isFullGc = Collections.indexOfSubList(
+          types, ImmutableList.of("ParNew", "CMS")) != -1;
+      return isFullGc ? GcEvent.LogType.FULL_GC : GcEvent.LogType.MINOR_GC;
     }
     throw new IllegalArgumentException("Log type must be specified. Check the log.");
   }
